8a2566fd20c93cabfbfcfecf1d931c9cf9540ed2
Jaya-shukla-cse <jaya.shukla_cs18@gla.ac.in>
2020-02-23 12:49:50 +0530

    make class MyContact and override the methods

---
+++ src/definition/MyContact.java
@@ -0,0 +1,31 @@
+package definition;
+
+
+import adt.Mycont;
+
+import java.io.InputStream;
+import java.util.Scanner;
+public class MyContact implements Mycont {
+    Scanner sc=new Scanner(System.in);
+
+    @java.lang.Override
+    public void addContact() {
+
+    }
+
+    @java.lang.Override
+    public void viewContact() {
+
+    }
+
+    @java.lang.Override
+    public void deleteContact() {
+
+    }
+
+    @java.lang.Override
+    public void searchContact() {
+
+    }
+}
+
