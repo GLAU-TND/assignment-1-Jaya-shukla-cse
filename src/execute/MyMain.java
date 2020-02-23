6be4cb4938c192e2de86558af69b92b117f1b01b
Jaya-shukla-cse <jaya.shukla_cs18@gla.ac.in>
2020-02-24 00:44:08 +0530

    made object of MyContact class

--- src/execute/MyMain.java
+++ src/execute/MyMain.java
@@ -4,5 +4,9 @@ import definition.MyContact;
 import java.util.Scanner;
 
 public class MyMain {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyContact user = new MyContact();
+        boolean response = true;
+    }
 }
